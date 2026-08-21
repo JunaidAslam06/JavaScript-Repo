037a85d34599bb5663502c52bc0dea05a4613c00
Junaid Aslam <junaidaslam2311@gmail.com>
2026-08-21 16:17:36 +0000

    feat: add variables and data types practice

---
+++ variables.js
@@ -0,0 +1,24 @@
+const accountId = 11280
+let accountAddress = "homeabcstreet"
+var accountEmail = "myemail.com"
+let accountUsername = "coding_account"
+
+
+console.table([accountId, accountAddress, accountEmail, accountUsername]);
+
+// Data Types 
+
+// numbers
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+// object
+
+console.log(typeof "number");
+
+
+
+
