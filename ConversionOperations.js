df1a5a3c56e2992dcbf348d3da109ea89713d794
Junaid Aslam <junaidaslam2311@gmail.com>
2026-08-22 12:31:26 +0000

    Conversion of Data Types

---
+++ ConversionOperations.js
@@ -0,0 +1,34 @@
+let age = true
+
+console.log(typeof age);
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// Notes
+
+// "20" => 20
+// "20abc" => NaN (Not a Number)
+// null => 0
+// true => 1
+// false => 0
+
+let isLoggedIn = "junaid"
+
+let BooleanLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanLoggedIn);
+
+// Notes
+// 1 => true 
+// 0 => false
+// "" => false
+// "junaid" => true
+
+let num = "119"
+
+let stringNumber = String(num)
+console.log(typeof stringNumber);
+
+
