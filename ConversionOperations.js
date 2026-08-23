95035288954daf2a7f54ec395679388e7e620c6a
Junaid Aslam <junaidaslam2311@gmail.com>
2026-08-23 09:09:24 +0000

    Conversion Operations

--- ConversionOperations.js
+++ ConversionOperations.js
@@ -32,3 +32,53 @@ let stringNumber = String(num)
 console.log(typeof stringNumber);
 
 
+// *********************** Operations*************************
+
+let value = 6
+let negVal = -value
+console.log(negVal);
+
+// Basic Arithmetic Operations
+// 2+2
+// 2-2
+// 2*2
+// 2/2
+// 2%2
+
+let str1 = "Hello"
+let str2 = " World!"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1+2+"2");
+
+// Tricky Conversions (highly unappriciated)
+
+console.log(+true);
+console.log(-true);
+console.log(+false);
+console.log(-false);
+
+console.log(+"");
+
+// Code readability and simplification is most noticed and important factor, don't ever code like this
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+
+
+// Prefix and Postfix assignment operator 
+
+let counter = 206
+++counter;
+console.log(counter);
+
+let newcounter = 310
+newcounter++;
+console.log(newcounter);
+
